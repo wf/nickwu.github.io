89015a819493a7a67deba926a66f8d9aaafa32d7
Wu Feng <wcf163@gmail.com>
2016-04-24 19:19:42 +0800

    test: reback jump.js

--- test/jump_orig.js
+++
@@ -1,3 +0,0 @@
-(function () {
-  window.opener.location.href = 'http://nickwu.net/test/z/?rsv_spt=1&issp=1&f=8&rsv_bp=0&rsv_idx=2…pg&rsv_enter=1&rsv_sug3=17&rsv_sug1=7&rsv_sug2=0&inputT=8700&rsv_sug4=8700';
-})();
